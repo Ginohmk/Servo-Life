247e791ff5352e2d3558f4a0b244c72495c9ad9f
ginohmk <michotall95@gmail.com>
2023-04-20 21:51:36 +0000

    feat: Add nav desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['*'],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {},
+    colors: {
+      primary_color: '#122182',
+    },
+    fontFamily: {
+      Playfair: ['"Playfair Display"', 'serif'],
+    },
   },
   plugins: [],
 };
