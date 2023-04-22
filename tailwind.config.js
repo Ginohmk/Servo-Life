e14637e23db8763184dbb3e7bc0755f054a80b35
ginohmk <michotall95@gmail.com>
2023-04-22 15:49:51 +0000

    add: Add cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,20 @@ module.exports = {
     fontFamily: {
       Playfair: ['"Playfair Display"', 'serif'],
     },
+    width: {
+      '90%': '90%',
+    },
     height: {
       '98vh': '98vh',
     },
     textColor: {
       white: '#ffffff',
+      primary_color: '#122182',
     },
     boxShadow: {
       primary_shadow: '0px 4px 58px rgba(18, 33, 130, 0.66)',
+      card_shadow: '0px 4px 58px rgba(18, 33, 130, 0.25)',
+      default: '0px 3px 8px rgba(0, 0, 0, 0.24)',
     },
   },
   plugins: [],
