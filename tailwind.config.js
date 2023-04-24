6e9a2b5ca9bbb99c22b674a71ad955bf09649d38
ginohmk <michotall95@gmail.com>
2023-04-24 17:32:54 +0000

    feat: Add extention of css tailwind styles to tails config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,38 +2,39 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
-    colors: {
-      primary_color: '#122182',
-      my_gray: '#787878',
-      white: '#ffffff',
-    },
-    fontFamily: {
-      Playfair: ['"Playfair Display"', 'serif'],
-    },
-    width: {
-      '90%': '90%',
-      5: '5px',
-      250: '250px',
-      16: '16px',
-    },
-    height: {
-      '98vh': '98vh',
-      110: '110px',
-      15.56: '15.56px',
-    },
-    gridTemplateColumns: {
-      grid_sizer: 'repeat(auto-fit, minmax(240px, 1fr))',
-      grid_right_larger: '1fr 2fr',
-    },
-    textColor: {
-      white: '#ffffff',
-      primary_color: '#122182',
-    },
-    boxShadow: {
-      primary_shadow: '0px 4px 58px rgba(18, 33, 130, 0.66)',
-      card_shadow: '0px 4px 58px rgba(18, 33, 130, 0.25)',
-      default: '0px 3px 8px rgba(0, 0, 0, 0.24)',
+    extend: {
+      colors: {
+        primary_color: '#122182',
+        my_gray: '#787878',
+        white: '#ffffff',
+      },
+      fontFamily: {
+        Playfair: ['"Playfair Display"', 'serif'],
+      },
+      width: {
+        '90%': '90%',
+        5: '5px',
+        250: '250px',
+        16: '16px',
+      },
+      height: {
+        '98vh': '98vh',
+        110: '110px',
+        15.56: '15.56px',
+      },
+      gridTemplateColumns: {
+        grid_sizer: 'repeat(auto-fit, minmax(240px, 1fr))',
+        grid_right_larger: '1fr 2fr',
+      },
+      textColor: {
+        white: '#ffffff',
+        primary_color: '#122182',
+      },
+      boxShadow: {
+        primary_shadow: '0px 4px 58px rgba(18, 33, 130, 0.66)',
+        card_shadow: '0px 4px 58px rgba(18, 33, 130, 0.25)',
+        default: '0px 3px 8px rgba(0, 0, 0, 0.24)',
+      },
     },
   },
   plugins: [],
